4cb01c84309be6fd61a29b11e79def621830d344
Sydney Ward <saw170030@utdallas.edu>
2020-11-04 15:10:41 -0600

    does it work??

--- app/ad_ldap.py
+++ app/ad_ldap.py
@@ -5,9 +5,9 @@ pyad.set_defaults(ldap_server="WIN-3QMNF1PAO8B.testlab.local", username="ajoseph
 user=pyad.aduser.ADUser.from_cn("Susan Sally")
 #users =pyad.aduser.ADUser()
 #users = pyad.adgroup.ADGroup.from_cn("User")
-#laborWebappGroup =  pyad.adgroup.ADGroup.from_cn("Labor Webapp")
+#laborWebappGroup = pyad.adgroup.ADGroup.from_cn("Labor Webapp")
 
-#from pyad import adquery
+# from pyad import adquery 
 qobj = adquery.ADQuery()
 qobj.execute_query(
 attributes = ["distinguishedName", "description"],
